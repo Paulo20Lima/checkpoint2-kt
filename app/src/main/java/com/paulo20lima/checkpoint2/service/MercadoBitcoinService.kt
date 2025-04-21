0f4c710f90665e37f62823e4d597afed7fc9dd65
Paulo Lima <66449714+Paulo20Lima@users.noreply.github.com>
2025-04-21 17:06:41 -0300

    first commit

---
+++ app/src/main/java/com/paulo20lima/checkpoint2/service/MercadoBitcoinService.kt
@@ -0,0 +1,10 @@
+package com.paulo20lima.checkpoint2.service
+import com.paulo20lima.checkpoint2.model.TickerResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface MercadoBitcoinService {
+
+    @GET("api/BTC/ticker/")
+    suspend fun getTicker(): Response<TickerResponse>
+}
